7af35dd7e5e5e71c8dc082ba592f6413d7dfb330
rischkong <rischko@visuelle.de>
2019-01-30 15:17:21 +0100

    Update lottieAnimationView.component.ts

--- src/lottieAnimationView.component.ts
+++ src/lottieAnimationView.component.ts
@@ -44,7 +44,9 @@ export class LottieAnimationViewComponent implements OnInit {
 
         this.viewWidth = this.width + 'px' || '100%';
         this.viewHeight = this.height + 'px' || '100%';
-
+        // DONE: Safari BUG
+        lottie.setLocationHref(document.location.href);
+        
         let anim: any = lottie.loadAnimation(this._options);
         this.animCreated.emit(anim);
     }
